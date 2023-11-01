97d9a15a3d98b89c46157e878649fb466fe09220
Imran <imran@DESKTOP-U1F9AG3>
2023-11-01 17:16:51 +0200

    Declare enemy class in enemy.hpp

---
+++ src/enemy.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+// TODO: Implement methods
+class Enemy {
+private:
+    int speed_;
+    int value_;
+    int price_;
+    int maxHP_;
+public:
+    explicit Enemy(int speed, int value, int price, int maxHP)
+            : speed_(speed), value_(value), price_(price), maxHP_(maxHP) {};
+    bool isAlive() const;
+};
