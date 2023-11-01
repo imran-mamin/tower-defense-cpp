ebd244b442bb4b96b95e4b212100fa3b8a9d0a97
Imran <imran@DESKTOP-U1F9AG3>
2023-11-01 17:04:22 +0200

    Declare Tower class, methods not defined yet

---
+++ src/tower.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+// TODO: Implement methods
+class Tower {
+private:
+    int radius_;
+    // int fireRate(game ticks);
+public:
+    
+    /**
+     * @brief Construct a new Tower object.
+     * 
+     * @param radius the radius of damage caused by projectile (int)
+    */
+    explicit Tower(int radius) : radius_(radius) {};
+    
+    const std::vector<Enemy> getEnemiesWithinRadius();
+    
+    /**
+     * @brief Destroy the Tower object.
+     */
+    virtual ~Tower() {};
+
+    virtual void update() = 0;
+    virtual void fire() = 0;
+    virtual bool takeDamage() = 0;
+    virtual void onDestroy() = 0;
+    int fireIntervalCounter() const;
+    virtual void sell() = 0;
+
+};
