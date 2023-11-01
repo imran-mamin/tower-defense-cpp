89e898aef05cbb617fd7517115ab60b2b39303da
Leevi Salonen <leevi.salonen@aalto.fi>
2023-11-01 18:19:05 +0200

    Add MapParserError class, derived from std::exception.

--- src/map_parser.hpp
+++ src/map_parser.hpp
@@ -2,20 +2,33 @@
 #pragma once
 
 #include <cstdint>
+#include <exception>
 #include <string>
 #include <tuple>
 #include <vector>
 
 #include "gamegrid.hpp"
 
-struct MapInfo{
-	/* tileWidth = tileHeight */
-	const std::uint8_t tileWidth;
-	const std::uint8_t mapWidth;
-	const std::uint8_t mapHeight;
-	std::vector<std::vector<std::uint16_t>> backgroundTiles;
-	/* Enemy path consisting from vectors. */
-	std::vector<std::tuple<std::uint32_t>> enemyPath;
+struct MapInfo {
+    /* tileWidth = tileHeight */
+    const std::uint8_t tileWidth;
+    const std::uint8_t mapWidth;
+    const std::uint8_t mapHeight;
+    std::vector<std::vector<std::uint16_t>> backgroundTiles;
+    /* Enemy path consisting from vectors. */
+    std::vector<std::tuple<std::uint32_t>> enemyPath;
+};
+
+class MapParserError : public std::exception {
+   public:
+    MapParserError(const std::string &what) : what_(std::string("MapParserError: ") + what) {}
+
+    const char *what() const noexcept override {
+		return what_.c_str();
+	}
+
+   private:
+    const std::string what_;
 };
 
 MapInfo ParseMap(const std::string &path);
