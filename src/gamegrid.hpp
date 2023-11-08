6620856d9147338f4181aed2525be8670c0cfe63
Imran <imran@DESKTOP-U1F9AG3>
2023-11-08 14:51:17 +0200

    Change Vector to std::vector<> in gamegrid.hpp

--- src/gamegrid.hpp
+++ src/gamegrid.hpp
@@ -31,7 +31,7 @@ class GameGrid {
 	std::uint8_t tileWidth_;
 	std::vector<sf::Sprite>
 	/* Vector of pairs containing tiles associated with a sprite. */
-	std::vector<Vector<std::pair<Tile, const &sf::Sprite>>> backgroundTilesAndSprites_;
+	std::vector<std::vector<std::pair<Tile, const &sf::Sprite>>> backgroundTilesAndSprites_;
     std::uint32_t width_;
     std::uint32_t height_;
 };
