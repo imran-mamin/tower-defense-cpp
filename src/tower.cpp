786c8be972196fbbff96c1c07375c61b4458752b
Imran <imran@DESKTOP-U1F9AG3>
2023-11-19 21:35:45 +0200

    Add comment, how to define enemiesWithinRadius method.

--- src/tower.cpp
+++ src/tower.cpp
@@ -1,5 +1,8 @@
 #include "tower.hpp"
 
+// Calculate the distance between this and the enemy object using formula:
+// sqrt((x_1 - x_2)^2 + (y_1 - y_2)^2)
+// If it is less than radius, then add it to the vector of enemies within the radius.
 const std::vector<Enemy> Tower::getEnemiesWithinRadius() {
 
 }
