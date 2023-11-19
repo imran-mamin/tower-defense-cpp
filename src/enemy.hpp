4a98642538a0a4b8634c2e63edfb2dec2095e9fd
Imran <imran@DESKTOP-U1F9AG3>
2023-11-19 18:57:19 +0200

    Link GameObject class as parent for enemy.

--- src/enemy.hpp
+++ src/enemy.hpp
@@ -1,15 +1,16 @@
 #pragma once
+#include "gameobject.hpp"
 
 // TODO: Implement methods
-class Enemy {
+class Enemy: public GameObject {
 private:
     int speed_;
     int value_;
     int price_;
     int maxHP_;
 public:
-    explicit Enemy(int speed, int value, int price, int maxHP)
-            : speed_(speed), value_(value), price_(price), maxHP_(maxHP) {};
+    explicit Enemy(int speed, int value, int price, int maxHP, sf::Sprite sprite)
+            : speed_(speed), value_(value), price_(price), maxHP_(maxHP), GameObject(sprite) {};
     
     virtual ~Enemy() {};
 
