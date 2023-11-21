0d71ebaaf4fe5b33a1ab05396e8cc9145a61e974
Imran <imran@DESKTOP-U1F9AG3>
2023-11-22 00:06:48 +0200

    Set GameObject class as friend to Game class to enable use of protected members.

--- src/game.hpp
+++ src/game.hpp
@@ -6,20 +6,17 @@
 
 class GameObject;  // Forward declaration
 
-
 class Game {
  public:
   Game(const GameGrid& grid);
 
   void AddObject(GameObject obj);
 
-  std::vector<GameObject> Objects() const;
-
-  GameGrid& GetGrid();
-
- protected:
-  std::size_t playerMoney_;
-  std::size_t enemyMoney_;
-  GameGrid grid_;
-  std::vector<GameObject> objects_;
+protected:
+    friend class GameObject;
+    std::size_t playerMoney_;
+    std::size_t enemyMoney_;
+    GameGrid grid_;
+    std::vector<GameObject> objects_;
 };
+
--- src/gameobject.hpp
+++ src/gameobject.hpp
@@ -15,4 +15,19 @@ protected:
     int health_;
     Game& game_;
     Pos position_;
+    std::size_t getPlayerMoney() {
+        return game_.playerMoney_;
+    }
+
+    std::size_t getEnemyMoney() {
+        return game_.enemyMoney_;
+    }
+   
+    GameGrid getGrid() {
+        return game_.grid_;
+    }
+
+    std::vector<GameObject> getObjects() {
+        return game_.objects_;   
+    }
 };
