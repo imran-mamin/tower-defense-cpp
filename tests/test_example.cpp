65528d97128d578368e383f25a13b037aac8aee8
Imran <imran@DESKTOP-U1F9AG3>
2023-11-25 14:17:07 +0200

    Continue link the unit tests

--- tests/test_example.cpp
+++ tests/test_example.cpp
@@ -1,5 +1,21 @@
 #include <gtest/gtest.h>
 
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <list>
+#include <optional>
+#include <vector>
+
+#include "cannon.hpp"
+#include "game.hpp"
+#include "gamegrid.hpp"
+#include "gameloop.hpp"
+#include "gameobject.hpp"
+#include "map_tile_selector_renderer.hpp"
+#include "testmapinfo.hpp"
+#include "tower.hpp"
+
+
 TEST(MyClassTest, TestConstructor) {
     EXPECT_EQ(1, 1);
 }
