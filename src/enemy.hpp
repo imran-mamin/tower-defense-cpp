057eb6fec94ab6152ab0bf57290856d7f9d81cad
Imran <imran@DESKTOP-U1F9AG3>
2023-11-26 14:09:38 +0200

    Improve enemy class structure

--- src/enemy.hpp
+++ src/enemy.hpp
@@ -7,10 +7,10 @@ protected:
     double speed_;
     int value_;
     int price_;
-    int maxHP_;
+    // int maxHP_;
 public:
     explicit Enemy(int speed, int value, int price, int maxHP, sf::Sprite sprite, Game& game, Pos position)
-            : GameObject(sprite, game, position), speed_(speed), value_(value), price_(price), maxHP_(maxHP) {};
+            : GameObject(sprite, game, position, maxHP), speed_(speed), value_(value), price_(price) {};
     
     virtual ~Enemy() {};
 
