239009e2aef34b23c7aca9dc52c5b74778e2d39e
Imran <imran.mamin@aalto.fi>
2023-11-28 22:27:02 +0200

    Improve update()-method in Bullet-class.

--- src/bullet.cpp
+++ src/bullet.cpp
@@ -11,17 +11,13 @@ void Bullet::update() {
     // Check, whether the enemy is within missile radius on every position update.
     int i = 0;
 
-    // TODO: Hit another enemy, if it appears earlier on the Bullet's path.
+    
     while (i < this->travel_speed()) {
-        // Calculate the distance between bullet and target.
-        // Formula sqrt((x_1 - x_2)^2 + (y_1 - y_2)^2)
-        Pos currPos = this->getPosition();
-        double xPow2 = std::pow((this->target_->getPosition().x - currPos.x), 2);
-        double yPow2 = std::pow((this->target_->getPosition().y - currPos.y), 2);
-        double dist = sqrt(xPow2 + yPow2);
+        // Hit the first enemy on the Bullet's path, if there is one.
+        std::vector<Enemy*> enemies = this->getEnemiesWithinRadius();
 
-        if (dist <= this->radius()) {
-            this->target_->takeDamage(this->damage());
+        if (!enemies.empty()) {
+            enemies.at(0)->takeDamage(this->damage());
             this->onDestroy();
         }
 
