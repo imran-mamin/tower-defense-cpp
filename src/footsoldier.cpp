32e0c237d19dd43a3fc23051f7692e0f03b943b0
Imran <imran.mamin@aalto.fi>
2023-11-29 00:34:31 +0200

    Improve code in update()-method.

--- src/footsoldier.cpp
+++ src/footsoldier.cpp
@@ -9,25 +9,16 @@ void FootSoldier::update() {
         // Find the vector2D that contains enemy's current position.
         while (i < (int)path.size()) {
             const Vec2D currVec = path.at(i);
-            std::cout << "currVecA: (x, y) = (" << currVec.a.x << ", " << currVec.a.y << ")" << std::endl;
 
-            // start position of the current vector to current position of the enemy.
-            Vec2D sPosToEnemPos;
-            sPosToEnemPos.a = currVec.a;
-            sPosToEnemPos.b = this->position_;
-            std::cout << "sPosStart: (x, y) = (" << sPosToEnemPos.a.x << ", " << sPosToEnemPos.a.y << ")" << std::endl;
-            std::cout << "sPosEnem: (x, y) = (" << sPosToEnemPos.b.x << ", " << sPosToEnemPos.b.y << ")" << std::endl;
+            // start position of the currVec to current position of the enemy.
+            Vec2D sPosToEnemPos = Vec2D{ currVec.a, this->position_ };
+
             // Find a current vector in form of (4i + 2j) for example, which is a point.
-            Pos vec1;
-            vec1.x = currVec.b.x - currVec.a.x;
-            vec1.y = currVec.b.y - currVec.a.y;
-            std::cout << "vec1: (x, y) = (" << vec1.x << ", " << vec1.y << ")" << std::endl;
+            Pos vec1 = Pos{ (currVec.b.x - currVec.a.x), (currVec.b.y - currVec.a.y) };
+
             // Find a vector between start position and enemy's position.
-            Pos vec2;
-            vec2.x = sPosToEnemPos.b.x - sPosToEnemPos.a.x;
-            vec2.y = sPosToEnemPos.b.y - sPosToEnemPos.a.y;
+            Pos vec2 = Pos{ (sPosToEnemPos.b.x - sPosToEnemPos.a.x), (sPosToEnemPos.b.y - sPosToEnemPos.a.y)};
             
-            std::cout << "vec2: (x, y) = (" << vec2.x << ", " << vec2.y << ")" << std::endl;
             // Calculate the dot product between vec1 and vec2.
             // If enemy's position is on current vector, then the angle between the vectors should be 0.
             // This means that the cosine should equal 1.
@@ -35,9 +26,6 @@ void FootSoldier::update() {
             // Calculate the length of the vectors vec1 and vec2.
             const double vec1Length = sqrt((vec1.x * vec1.x) + (vec1.y * vec1.y));
             const double vec2Length = sqrt((vec2.x * vec2.x) + (vec2.y * vec2.y));
-
-            std::cout << "vec1Length: " << vec1Length << std::endl;
-            std::cout << "vec2Length: " << vec2Length << std::endl;
 
             const double cos12 = dotP / (vec1Length * vec2Length);
             // Check the angle and the vec2 should not be null vector.
@@ -54,9 +42,62 @@ void FootSoldier::update() {
                 continue;
             }
 
+            // Find unit vector of vec1.
+            Pos unitVec1 = Pos{ (vec1.x / vec1Length), (vec1.y / vec1Length) };
+            
+            // Advance enemy's position.
+            this->position_.x += this->speed_ * unitVec1.x;
+            this->position_.y += this->speed_ * unitVec1.y;
+            
+            std::cout << "enemyPos (x, y) = (" << this->position_.x << ", " << this->position_.y << ")" << std::endl;
+            // Check that the enemy doesn't go further than the end point of a path vector.
+            bool outX = (unitVec1.x > 0) ? (this->position_.x > currVec.b.x) : (this->position_.x < currVec.b.x);
+            bool outY = (unitVec1.y > 0) ? (this->position_.y > currVec.b.y) : (this->position_.y < currVec.b.y);
+            if (outX || outY) {
+                
+                // Check how far does the enemy go from the currVec's end point.
+                double outInSpeed = (unitVec1.x != 0) ? std::abs((this->position_.x - currVec.b.x) / unitVec1.x) : std::abs((this->position_.y - currVec.b.y) / unitVec1.y);
+                // Restore enemy's position to end point of the currVec.
+                this->position_ = currVec.b;
+                std::cout << "unitVec1.x = " << unitVec1.x << std::endl;
+                std::cout << "unitVec1.y = " << unitVec1.y << std::endl;
+                std::cout << "outInSpeed = " << outInSpeed << std::endl;
+                std::cout << "new position = (" << this->position_.x << ", " << this->position_.y << ")" << std::endl;
+                int j = i + 1;
+
+                while ((j < (int)path.size()) && (outInSpeed > 0)) {
+                    const Vec2D pathPoints = path.at(j);
+                    Pos pathVec = Pos{ (pathPoints.b.x - pathPoints.a.x), (pathPoints.b.y - pathPoints.a.y) };
+                    double pathVecLength = sqrt((pathVec.x * pathVec.x) + (pathVec.y * pathVec.y));
+                    Pos unitPathVec = Pos{ (pathVec.x / pathVecLength), (pathVec.y / pathVecLength) };
+                    
+                    std::cout << "unitPathVec.x = " << unitPathVec.x << std::endl;
+                    std::cout << "unitPathVec.y = " << unitPathVec.y << std::endl;
+                    // Update enemy's position.
+                    this->position_.x += outInSpeed * unitPathVec.x;
+                    this->position_.y += outInSpeed * unitPathVec.y;
+
+                    // If it goes also out of pathPoints vec2D then continue.
+                    bool isOutX = (unitPathVec.x > 0) ? (this->position_.x > pathPoints.b.x) : (this->position_.x < pathPoints.b.x);
+                    bool isOutY = (unitPathVec.y > 0) ? (this->position_.y > pathPoints.b.y) : (this->position_.y < pathPoints.b.y);
+                    
+                    if (isOutX || isOutY) {
+                        std::cout << "a" << std::endl;
+                        // Remove the length of pathVec from the outInSpeed.
+                        outInSpeed -= (unitPathVec.x != 0) ? std::abs((this->position_.x - pathPoints.b.x) / unitPathVec.x) : std::abs((this->position_.y - pathPoints.b.y) / unitPathVec.y);
+                        j++;
+                        continue;
+                    }
+                    // If it doesn't go into if-block, then the next path vector2D is found.
+                    outInSpeed = 0;
+                }
+                
+                assert(outInSpeed <= 0); // This is for TODO above.
+            }
+            /*
             // Tells, whether the enemy goes backward or forward.
             bool goesLeftOrUp = false;
-
+            
             // Advance enemy's position depending on the vec1 direction.
             if (vec1.x != 0) {
                 this->position_.x += (vec1.x > 0) ? this->speed_ : -this->speed_;
@@ -170,16 +211,12 @@ void FootSoldier::update() {
 
                     assert(out <= 0); // This is for TODO above.
                 }
-            }
-            std::cout << std::endl;
-            std::cout << "Enemy updated position" << std::endl;
-            std::cout << "(x, y) = (" << this->position_.x << ", " << this->position_.y << ")" << std::endl;
-            std::cout << std::endl;
+            }   */
             break;
         }
     } else {
         // Remove enemy object from vector of objects.
-        this->game_.DeleteObject(static_cast<GameObject*>(this));// this->getObjects().begin();
+        this->game_.DeleteObject(static_cast<GameObject*>(this));
 
         // Add profit to player.
         this->addPlayerMoney(this->value_);
@@ -190,5 +227,5 @@ void FootSoldier::update() {
 }
 
 void FootSoldier::onDestroy() {
-    this->~FootSoldier();
+    this->game_.DeleteObject(this);
 }
