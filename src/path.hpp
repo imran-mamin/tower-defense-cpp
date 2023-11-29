d9d15e4200aaa2c017ef19a18fca71bd564199a2
Leevi Salonen <leevi.salonen@aalto.fi>
2023-11-29 12:41:28 +0200

    Add missing path specifier.

--- src/path.hpp
+++ src/path.hpp
@@ -3,5 +3,6 @@
 
 #include <string>
 
+const std::string TILE_ROOT_DIRECTORY = std::string("../rsrc/tiles/");
 const std::string BACKGROUND_TILE_DIRECTORY = std::string("../rsrc/tiles/background/");
 
