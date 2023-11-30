7552b6102ba0b6cfb115124264f416dc85c9dfba
Leevi Salonen <leevi.salonen@aalto.fi>
2023-11-30 15:43:15 +0200

    Add enum classes for enemies and weapons.

---
+++ src/weapons_and_enemies.hpp
@@ -0,0 +1,18 @@
+
+#pragma once
+
+#include <map>
+enum class Weapons{
+	GreenCannon,
+	RedCannon,
+	MissileLauncher1,
+	MissileLauncher2,
+	MissileLauncher3
+};
+
+enum class Enemies{
+	// TODO
+};
+
+std::map<typename Key, typename Tp>
+
