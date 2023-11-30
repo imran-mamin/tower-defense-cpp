b8c2947f84d0625f4ad517e26b078a1a8d71465b
Leevi Salonen <leevi.salonen@aalto.fi>
2023-11-30 16:42:56 +0200

    Add mapping from enemies and weapons to correct tile id's.

--- src/weapons_and_enemies.hpp
+++ src/weapons_and_enemies.hpp
@@ -1,7 +1,10 @@
 
 #pragma once
 
+#include <cstdint>
 #include <map>
+#include <utility>
+
 enum class Weapons{
 	GreenCannon,
 	RedCannon,
@@ -11,8 +14,32 @@ enum class Weapons{
 };
 
 enum class Enemies{
-	// TODO
+	Soldier1,
+	Soldier2,
+	Soldier3,
+	Soldier4,
+	Tank1,
+	Tank2,
+	Plane1,
+	Plane2
 };
 
-std::map<typename Key, typename Tp>
+const std::map<Weapons, std::uint16_t> weaponToTileIDMapping = {
+	std::make_pair(Weapons::GreenCannon, 248),
+	std::make_pair(Weapons::RedCannon, 249),
+	std::make_pair(Weapons::MissileLauncher1, 205),
+	std::make_pair(Weapons::MissileLauncher2, 204),
+	std::make_pair(Weapons::MissileLauncher3, 203)
+};
+
+const std::map<Enemies, std::uint16_t> enemyToTileIDMapping = {
+	std::make_pair(Enemies::Soldier1, 244),
+	std::make_pair(Enemies::Soldier2, 245),
+	std::make_pair(Enemies::Soldier3, 246),
+	std::make_pair(Enemies::Soldier4, 247),
+	std::make_pair(Enemies::Tank1, 267),
+	std::make_pair(Enemies::Tank2, 268),
+	std::make_pair(Enemies::Plane1, 269),
+	std::make_pair(Enemies::Plane2, 270)
+};
 
