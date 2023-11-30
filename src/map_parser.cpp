41cd93268cf37a7d2d899f6d0bf2f908381c2788
Leevi Salonen <leevi.salonen@aalto.fi>
2023-11-30 14:43:54 +0200

    Finish the map parser.

--- src/map_parser.cpp
+++ src/map_parser.cpp
@@ -1,39 +1,122 @@
+#include <cmath>
+#include <cstddef>
 #include <cstdint>
 #include <exception>
 #include <fstream>
 #include <iostream>
+#include <stdexcept>
 #include <string>
+#include <vector>
 
 #include "json.hpp"
 #include "map_parser.hpp"
+#include "pos.hpp"
 #include "vec2d.hpp"
 
+
+const std::string tileWidthKey = "tile_width";
+const std::string mapWidthKey = "map_width";
+const std::string mapHeightKey = "map_height";
+const std::string backgroundTilesKey = "background";
+const std::string enemyPathKey = "enemy_path";
+const std::string enemyStartCashKey = "enemy_start_cash";
+const std::string enemyCashGrowthPerTickKey = "enemy_cash_growth_per_tick";
 
 MapInfo ParseMap(const std::string &path) {
 	std::ifstream is(path);
 	if (is.fail()) {
-		throw MapParserError(std::string("Failed to open file at '") + path + "'.");
+		throw MapParserError(std::string("Failed to open level map at '") + path + "'.");
 	}
-	nlohmann::json data = nlohmann::json::parse(is);
 
+	/* Create a parser instance. */
+	nlohmann::json data = nlohmann::json::parse(is);
 	try {
-		/* TODO: Read the enemy_path */
-		std::vector<Vec2D> enemy_path;
-		MapInfo mapInfo =  MapInfo{ data["tile_width"], data["map_width"], data["map_height"], data["background"], enemy_path };
-	
-		if (mapInfo.mapWidth * mapInfo.mapHeight != mapInfo.backgroundTiles.size()) {
+		std::uint8_t tileWidth;
+    	std::uint8_t mapWidth;
+    	std::uint8_t mapHeight;
+		std::vector<std::vector<std::uint16_t>> backgroundTiles;
+		std::vector<std::vector<std::vector<std::uint32_t>>> enemyPath_;
+		std::uint64_t enemyStartCash;
+		std::float_t enemyCashGrowthPerTick;
+
+		/* Read the input. */
+		data.at(tileWidthKey).get_to(tileWidth);
+		data.at(mapWidthKey).get_to(mapWidth);
+		data.at(mapHeightKey).get_to(mapHeight);
+		data.at(backgroundTilesKey).get_to(backgroundTiles);
+		data.at(enemyPathKey).get_to(enemyPath_);
+		data.at(enemyStartCashKey).get_to(enemyStartCash);
+		data.at(enemyCashGrowthPerTickKey).get_to(enemyCashGrowthPerTick);
+
+		/* ===== Validate the input. ===== */
+
+		/*
+		 * Make sure the background tile count matches the grid size and the widths and heights match.
+		 */
+		if (backgroundTiles.size() != mapHeight) {
+			throw MapParserError("Column-wise background tile count must be equal to the map height!");
+		}
+		std::size_t tileTotalCount = 0;
+		for (auto row : backgroundTiles) {
+			tileTotalCount += row.size();
+			if (row.size() != mapWidth) {
+				throw MapParserError("Row-wise background tile count must be equal to the map width!");
+			}
+		}
+		if (tileTotalCount != mapWidth * mapHeight) {
 			throw MapParserError("Tile count of the map must be equal to tileWidth * mapHeight!");
 		}
 
-		return mapInfo;
+		/*
+		 * Validate the enemy path vector.
+		 */
+		if (enemyPath_.size() < 1) {
+			throw MapParserError("Empty enemy path!");
+		}
+		for (auto vec : enemyPath_) {
+			/* Check that the vectors (only) have start point (a) and the ending point (b). */
+			if (vec.size() != 2) {
+				throw MapParserError("Error in the enemy path. Each vector must have both a starting point and an ending point and nothing else.");
+			}
+			/* Check for each point that: that the points have only the x and y values. */
+			for (auto point : vec) {
+				/* the point has only x and y value, */
+				if (point.size() != 2) {
+					throw MapParserError("Error in one of the points of one of the enemy vectors. Each point must have its x and y value and nothing else.");
+				}
+				/* and that the point is within the level bounds. */
+				if (point[0] > tileWidth * mapWidth) {
+					throw MapParserError(std::string("Erroneous value for x. Value ") + std::to_string(point[0]) + " is not within the level bounds.");
+				}
+				if (point[1] > tileWidth * mapHeight) {
+					throw MapParserError(std::string("Erroneous value for y. Value ") + std::to_string(point[1]) + " is not within the level bounds.");
+				}
+			}
+		}
+		// TODO
+		/* Make sure the enemy path starts from a legal value.  */
+		//if (enemyPath_[0][0][0] == 0 && enemyPath_[0][0][]) {
+		//	
+		//}
+		/* TODO: Make sure the enemy path and ends to a legal value.  */
+		/* TODO: Make sure that the vectors come in order. */
+
+		/* Convert the enemy path into the Vec2D format. */
+		std::vector<Vec2D> enemyPath;
+		for (auto vec : enemyPath_) {
+			Pos a{ vec[0][0], vec[0][1] };
+			Pos b{ vec[1][0], vec[1][1] };
+
+			enemyPath.push_back(Vec2D{ a, b });
+		}
+
+		return MapInfo{ tileWidth, mapWidth, mapHeight, backgroundTiles, enemyPath, enemyStartCash, enemyCashGrowthPerTick };
 	}
+	/* TODO: Catch correct exceptions: missing field and incorrect data format. */
 	catch (const std::exception &e) {
+		std::cout << e.what() << std::endl;
 		// TODO: Implement new exception throwing besides just rethrowing.
 		throw e;
 	}
-	// TODO: More precise error output.
-	//catch (/* TODO: Catch correct exception (trying to read data field that is incorrect, or type error idk.) */) {
-	//	throw MapParserError("TODO");
-	//}
 }
 
