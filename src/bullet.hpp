b88271ac3b2283a6db6e56ed4890c666b600a507
Imran <imran.mamin@aalto.fi>
2023-12-06 11:56:11 +0200

    Change parameter list in Bullet class

--- src/bullet.hpp
+++ src/bullet.hpp
@@ -7,13 +7,12 @@
 // TODO: Implement methods
 class Bullet : public Projectile {
 private:
-  Enemy* target_;
   Pos endPos_;
   Pos startPos_;
 
 public:
-  Bullet(Game& game, Enemy* target, int travel_speed, int damage, int radius, Pos position, Pos endPos)
-      : Projectile(game, travel_speed, damage, radius, position), target_(target), endPos_(endPos), startPos_(position) {};
+  Bullet(Game& game, int travel_speed, int damage, int radius, Pos position, Pos endPos)
+      : Projectile(game, travel_speed, damage, radius, position), endPos_(endPos), startPos_(position) {};
 
   // Destructor implementation
   ~Bullet() override {}
