0494c248d50c1f6fbda734abf553fb64daa10255
Imran <imran.mamin@aalto.fi>
2023-12-06 23:42:03 +0200

    Implement the fire()-method in Cannon class

--- src/cannon.cpp
+++ src/cannon.cpp
@@ -38,7 +38,7 @@ int findCurrentPathVec(const Enemy* e, const std::vector<Vec2D>& path) {
  * This method finds the length of path from enemy's current position to pos p according
  * to the enemypath (path parameter).
 */
-float findEnemyDist(const Enemy* e, std::vector<Vec2D> path, Pos p, int i) {
+float findEnemyDist(const Enemy* e, const std::vector<Vec2D>& path, Pos p, int i) {
     int j = i;
     
     // Distance to the end of current path.
@@ -57,9 +57,9 @@ float findEnemyDist(const Enemy* e, std::vector<Vec2D> path, Pos p, int i) {
 /**
  * This method returns the Vec2D, where the collision between bullet and enemy occurs.
 */
-Vec2D findCollisionVec(const Enemy* e, int i, const std::vector<Vec2D> path, Pos bulletPos, int bulletSpeed) {
+Vec2D findCollisionVec(const Enemy* e, int i, const std::vector<Vec2D>& path, Pos bulletPos, int bulletSpeed) {
 
-    // TODO: Check separately for path.at(i).
+    // Check separately for path.at(i).
     float distEnemy = sqrt(std::pow(path.at(i).b.x - e->getPosition().x, 2) + std::pow(path.at(i).b.y - e->getPosition().y, 2));
     float distBullet = sqrt(std::pow(path.at(i).b.x - bulletPos.x, 2) + std::pow(path.at(i).b.y - bulletPos.y, 2));
     float enemyTime = distEnemy / e->GetSpeed();
@@ -99,37 +99,46 @@ Vec2D findCollisionVec(const Enemy* e, int i, const std::vector<Vec2D> path, Pos
     }
 };
 
-float binarySearch(float start, float end) {
+Pos binarySearch(const Enemy* e, int i, const std::vector<Vec2D>& path, Pos start, Pos end, Pos bulletPos, int bulletSpeed, int radius) {
+    Pos left = start;
+    Pos mid;
+    mid.x = start.x + (end.x - start.x) / 2;
+    mid.y = start.y + (end.y - start.y) / 2;
+    Pos right = end;
 
+    while (((right.x - left.x) + (right.y - left.y)) > radius) {
+        float enemyDistToMidLen = findEnemyDist(e, path, mid, i);
+        // Calculate times for enemy to reach left, mid and right positions.
+        float tEnemM = enemyDistToMidLen / e->GetSpeed();
+
+        float bulletDistToMidLen = sqrt(std::pow(mid.x - bulletPos.x, 2) + std::pow(mid.y - bulletPos.y, 2));
+        // Calculate times for enemy to reach left mid and right positions.
+        float tBullM = bulletDistToMidLen / bulletSpeed;
+
+        if (tEnemM > tBullM) {
+            right = mid;
+            mid.x = left.x + (right.x - left.x) / 2;
+            mid.y = left.y + (right.y - left.y) / 2;
+        } else {
+            left = mid;
+            mid.x = left.x + (right.x - left.x) / 2;
+            mid.y = left.y + (right.y - left.y) / 2;
+        }
+    }
+
+    return mid;
+    
 }
 
-Pos findCollisionPos(const Enemy* e, Vec2D collisionVec, Pos bulletPos, int bulletSpeed, int radius) {
+Pos findCollisionPos(const Enemy* e, int i, const std::vector<Vec2D>& path, Vec2D collisionVec, Pos bulletPos, int bulletSpeed, int radius) {
     float diffX = collisionVec.b.x - collisionVec.a.x;
     float diffY = collisionVec.b.y - collisionVec.a.y;
 
-    float distFromA;
-    Pos collisionPos;
-
-    // Intialize distFromA variable according to the Vec2D direction.
-    if (diffX > 0) {
-        distFromA = binarySearch(collisionVec.a.x, collisionVec.b.x);
-        collisionPos.x = collisionVec.a.x + distFromA;
-        collisionPos.y = collisionVec.a.y;
-    } else if (diffY > 0) {
-        distFromA = binarySearch(collisionVec.a.y, collisionVec.b.y);
-        collisionPos.x = collisionVec.a.x;
-        collisionPos.y = collisionVec.a.y + distFromA;
-    } else if (diffX < 0) {
-        distFromA = binarySearch(collisionVec.b.x, collisionVec.a.x);
-        collisionPos.x = collisionVec.a.x - distFromA;
-        collisionPos.y = collisionVec.a.y;
-    } else { // else if (diffY < 0) {
-        distFromA = binarySearch(collisionVec.b.y, collisionVec.a.y);
-        collisionPos.x = collisionVec.a.x;
-        collisionPos.y = collisionVec.a.y - distFromA;
+    if ((diffX < 0) || (diffY < 0)) {
+        return binarySearch(e, i, path, collisionVec.b, collisionVec.a, bulletPos, bulletSpeed, radius);
     }
 
-    return collisionPos;
+    return binarySearch(e, i, path, collisionVec.a, collisionVec.b, bulletPos, bulletSpeed, radius);
 }
 
 /**
@@ -148,28 +157,12 @@ void Cannon::fire() {
     // Returns current vec2D and its index in enemyPath vector.
     int currIndex = findCurrentPathVec(e, game_.GetGrid().EnemyPath());
     Vec2D collisionVec = findCollisionVec(e, currIndex, game_.GetGrid().EnemyPath(), this->getPosition(), bulletSpeed);
-    Pos collisionPos = findCollisionPos(e, collisionVec, this->getPosition(), bulletSpeed, radius);
+    Pos collisionPos = findCollisionPos(e, currIndex, game_.GetGrid().EnemyPath(), collisionVec, this->getPosition(), bulletSpeed, radius);
 
-    /*
-    // This is constant - representing 18px motion per "time unit".
-    int bulletSpeed = 18;
-    // Find the vector from cannon to enemy.
-    Pos vecToEnem;
-    vecToEnem.x = e->getPosition().x - this->getPosition().x;
-    vecToEnem.y = e->getPosition().y - this->getPosition().y;
-
-    // Find the length of vecToEnem.
-    float vecToEnemLen = sqrt((vecToEnem.x * vecToEnem.x) + (vecToEnem.y * vecToEnem.y));
-    */
-    // Adjust for target position based on the amount of "time units" to travel "dist"
-    // and the targets speed vector.
-
-
-    // Bullet(Game& game, Enemy* target, int travel_speed, int damage, int radius, Pos position, Pos endPos)
-    // Create a Bullet-class instance.
-
-    // Add bullet to vector of objects.
-    // this->game_.AddObject(b);
+    // Bullet(Game& game, int travel_speed, int damage, int radius, Pos position, Pos endPos)
+    // TODO: The Bullet-instance should be added to the vector of objects.
+    // game_.AddObject(std::make_unique<Bullet>(Bullet(game_, bulletSpeed, 6, radius, position_, collisionPos)));
+    
 };
 
 void Cannon::update() {
