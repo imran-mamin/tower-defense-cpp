45d2a7a01b63bd679ce96210631226826b74f0a8
Imran <imran.mamin@aalto.fi>
2023-12-08 02:05:52 +0200

    Create hardcoded testMap for debugging purposes.

--- src/testmapinfo.cpp
+++ src/testmapinfo.cpp
@@ -1,4 +1,3 @@
-/*
 #include "testmapinfo.hpp"
 
 #include <cstdint>
@@ -13,6 +12,10 @@ const std::uint32_t mapWidth = 32;
 const std::uint32_t mapHeight = 12;
 
 MapInfo testMapInfoObject1() {
+        const std::uint8_t tileWidth = 64;
+        const std::uint8_t mapWidth = 20;
+        const std::uint8_t mapHeight = 12;
+        const std::uint64_t playerStartCash = 6000;
         //Create the background tiles.
         std::vector<std::vector<std::uint16_t>> backgroundTiles;
         for (std::uint32_t j = 0; j < mapHeight; j++) {
@@ -33,14 +36,27 @@ MapInfo testMapInfoObject1() {
 
         //TODO: Create better enemy path.
         std::vector<Vec2D> enemyPath;
+        enemyPath.push_back(Vec2D{ Pos{0, 640}, Pos{192, 640} });
+        enemyPath.push_back(Vec2D{ Pos{192, 640}, Pos{192, 128} });
+        enemyPath.push_back(Vec2D{ Pos{192, 128}, Pos{1088, 128} });
+        enemyPath.push_back(Vec2D{ Pos{1088, 128}, Pos{1088, 320} });
+        enemyPath.push_back(Vec2D{ Pos{1088, 320}, Pos{384, 320} });
+        enemyPath.push_back(Vec2D{ Pos{384, 320}, Pos{384, 640} });
+        enemyPath.push_back(Vec2D{ Pos{384, 640}, Pos{1280, 640} });
 
+        std::vector<std::pair<u_int16_t, std::pair<u_int16_t, u_int16_t>>> obstacleTiles = {{135, {0, 0}}, {134, {640, 512}}};
+        std::vector<EnemyType> enemyTypes;
+        std::vector<EnemyWave> enemyWaves;
+        for (int i = 0; i < 60; i++) {
+                enemyTypes.push_back(EnemyType::Soldier1);
+        }
+        enemyWaves.push_back(EnemyWave(60, enemyTypes));
+        /*
         enemyPath.push_back(Vec2D{ Pos{ 0, 224 }, Pos{ 60, 224 } });
         enemyPath.push_back(Vec2D{ Pos{ 60, 224 }, Pos{ 60, 260 } });
         enemyPath.push_back(Vec2D{ Pos{ 60, 260 }, Pos{ 0, 260 } });
         enemyPath.push_back(Vec2D{ Pos{ 0, 260 }, Pos{ 0, 224 } });
+        */
 
-        return MapInfo{ 64, 20, 12, backgroundTiles, enemyPath, 10000 };
+        return MapInfo{ tileWidth, mapWidth, mapHeight, backgroundTiles, obstacleTiles, playerStartCash, enemyPath, enemyWaves};
 }
-
-
-*/
--- src/testmapinfo.hpp
+++ src/testmapinfo.hpp
@@ -2,5 +2,8 @@
 #pragma once
 
 #include "map_parser.hpp"
+#include "enemy_wave.hpp"
+#include "weapons_and_enemies.hpp"
+
 MapInfo testMapInfoObject1();
 
