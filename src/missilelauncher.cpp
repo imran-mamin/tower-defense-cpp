64d22ed37bbf0909e93b9f594469253765657524
Aleksi Kalliomäki <aleksi.kalliomaki@aalto.fi>
2023-12-11 22:29:04 +0200

    missile launcher changes

--- src/missilelauncher.cpp
+++ src/missilelauncher.cpp
@@ -1,62 +1,63 @@
+#include "missilelauncher.hpp"
+
 #include <assert.h>
 
-#include "missilelauncher.hpp"
 #include "utility_func_game.hpp"
 
 void MissileLauncher::fire() {
-    assert(!this->getEnemiesWithinRadius().empty());
-    Enemy* e = this->getEnemiesWithinRadius().at(0);
+  assert(!this->getEnemiesWithinRadius().empty());
+  Enemy* e = this->getEnemiesWithinRadius().at(0);
 
-    // Missile(Game& game, int travel_speed, int damage, int radius, Enemy target, Pos position, double exp_radius = 0)
-    // Create a missile object.
-    Pos p = this->getPosition();
+  // Missile(Game& game, int travel_speed, int damage, int radius, Enemy target,
+  // Pos position, double exp_radius = 0) Create a missile object.
+  Pos p = this->getPosition();
 
-    // Add missile to vector.
-    addObject(game_, new Missile(game_, 6, 6, 18, e, p, 4)); 
+  // Add missile to vector.
+  addObject(game_, new Missile(game_, 6, 100, 180, e, p, 4));
 
-    // Find the rotation angle of the missilelauncher.
-    // Unit vector pointing to North according to the picture.
-    Pos unitVecN = Pos{ 0, -1 };
-    Pos MToEVec = Pos{ (e->getPosition().x - position_.x), (e->getPosition().y - position_.y) };
+  // Find the rotation angle of the missilelauncher.
+  // Unit vector pointing to North according to the picture.
+  Pos unitVecN = Pos{0, -1};
+  Pos MToEVec = Pos{(e->getPosition().x - position_.x),
+                    (e->getPosition().y - position_.y)};
 
-    float MToEVecLen = sqrt(std::pow(MToEVec.x, 2) + std::pow(MToEVec.y, 2));
-    // Find dot product of vectors.
-    float dotP = unitVecN.y * MToEVec.y;
-    float angleInRad = std::acos(dotP / MToEVecLen);
-    float angleInDeg = angleInRad * (180.0 / M_PI);
-    
-    // Is the enemy on the left side of the missilelauncher?
-    bool onTheLeft = (e->getPosition().x <= position_.x);
-    
-    if (onTheLeft) {
-        this->SetAngle(360.0 - angleInDeg);
-    } else {
-        this->SetAngle(angleInDeg);
-    }
+  float MToEVecLen = sqrt(std::pow(MToEVec.x, 2) + std::pow(MToEVec.y, 2));
+  // Find dot product of vectors.
+  float dotP = unitVecN.y * MToEVec.y;
+  float angleInRad = std::acos(dotP / MToEVecLen);
+  float angleInDeg = angleInRad * (180.0 / M_PI);
+
+  // Is the enemy on the left side of the missilelauncher?
+  bool onTheLeft = (e->getPosition().x <= position_.x);
+
+  if (onTheLeft) {
+    this->SetAngle(360.0 - angleInDeg);
+  } else {
+    this->SetAngle(angleInDeg);
+  }
 }
 
 void MissileLauncher::update() {
-    assert(this->fireIntervalCounter_ >= 0);
+  assert(this->fireIntervalCounter_ >= 0);
 
-    if (this->fireIntervalCounter_ == 0) {
-        // Check if there are enemies within the radius.
-        if (!this->getEnemiesWithinRadius().empty()) {
-            // Fire the first enemy in the radius.
-            this->fire();
+  if (this->fireIntervalCounter_ == 0) {
+    // Check if there are enemies within the radius.
+    if (!this->getEnemiesWithinRadius().empty()) {
+      // Fire the first enemy in the radius.
+      this->fire();
 
-            // Start projectile reloading.
-            fireIntervalCounter_ = fireRate_;
-        }
-    } else {
-        fireIntervalCounter_--;
+      // Start projectile reloading.
+      fireIntervalCounter_ = fireRate_;
     }
+  } else {
+    fireIntervalCounter_--;
+  }
 }
 
 int MissileLauncher::sell() {
-    int p = this->price_;
-    this->onDestroy();
-    return p;
+  int p = this->price_;
+  this->onDestroy();
+  return p;
 }
 
-void MissileLauncher::onDestroy() {
-}
+void MissileLauncher::onDestroy() {}
--- src/missilelauncher.hpp
+++ src/missilelauncher.hpp
@@ -1,32 +1,35 @@
 #pragma once
+#include <cstdint>
+
 #include "game.hpp"
 #include "missile.hpp"
 #include "tower.hpp"
-#include <cstdint>
 
 const std::uint32_t BasicMissileLauncherPrice = 8000;
 
 #include <cmath>
 
 class MissileLauncher : public Tower {
-   // TODO: Make protected.
-	//protected:
-	public:
-    MissileLauncher(int radius, int fireRate, int price, Game* game,
-		    Pos position)
-	: Tower(radius, fireRate, price, game, position, 250){};
+  // TODO: Make protected.
+  // protected:
+ public:
+  MissileLauncher(int radius, int fireRate, int price, Game* game, Pos position)
+      : Tower(radius, fireRate, price, game, position, 250){};
 
-   public:
-    ~MissileLauncher() = default;
+ public:
+  ~MissileLauncher() = default;
 
-    void fire();
-    void onDestroy();
-    void update();
-    int sell();
+  void fire();
+  void onDestroy();
+  void update();
+  int sell();
 };
 
 class BasicMissileLauncher : public MissileLauncher {
-   public:
-	BasicMissileLauncher(Game* game, Pos position) : MissileLauncher(64 * 10, 6, BasicMissileLauncherPrice, game, position) {}// MissileLauncher(64 * 10, 3, BasicMissileLauncherPrice, game, position) {}
+ public:
+  BasicMissileLauncher(Game* game, Pos position)
+      : MissileLauncher(64 * 10, 55, BasicMissileLauncherPrice, game,
+                        position) {
+  }  // MissileLauncher(64 * 10, 3, BasicMissileLauncherPrice, game, position)
+     // {}
 };
-
